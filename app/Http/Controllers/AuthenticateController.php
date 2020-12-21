e3419875ad3cc596ffce66181b7fc5c2e038e3c2
Theodore R. Smith <theodore@phpexperts.pro>
2020-12-20 21:07:22 -0600

    [m] Added all of the missing license headers.

--- app/Http/Controllers/AuthenticateController.php
+++ app/Http/Controllers/AuthenticateController.php
@@ -1,4 +1,15 @@
-<?php
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of The Resonate Wealth Game, a Bettergist Collective Project.
+ *
+ * Copyright © 2020 Theodore R. Smith <hopeseekr@gmail.com>
+ *   GPG Fingerprint: D8EA 6E4D 5952 159D 7759  2BB4 EEB6 CE72 F441 EC41
+ *   https://github.com/BettergistCo/ResonateWealthGame
+ *   https://www.resonance.how/wealth-game/
+ *
+ * This file is licensed under the Creative Commons Attribution v4.0 License.
+ */
 
 namespace App\Http\Controllers;
 
--- database/migrations/2020_12_13_175302_version_one_point_oh.php
+++ database/migrations/2020_12_13_175302_version_one_point_oh.php
@@ -11,6 +11,17 @@
  * This file is licensed under the Creative Commons Attribution v4.0 License.
  */
 
+/**
+ * This file is part of The Resonate Wealth Game, a Bettergist Collective Project.
+ *
+ * Copyright © 2020 Theodore R. Smith <hopeseekr@gmail.com>
+ *   GPG Fingerprint: D8EA 6E4D 5952 159D 7759  2BB4 EEB6 CE72 F441 EC41
+ *   https://github.com/BettergistCo/ResonateWealthGame
+ *   https://www.resonance.how/wealth-game/
+ *
+ * This file is licensed under the Creative Commons Attribution v4.0 License.
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
